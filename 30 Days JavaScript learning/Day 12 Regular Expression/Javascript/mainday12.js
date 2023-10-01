0a20dcaf5dffe209e51b57b2a84f6b8ec2f2f3d4
Ramzi_Akbar <110384679+MuniMunii@users.noreply.github.com>
2023-10-01 22:16:54 +0700

    Add files via upload

--- 30 Days JavaScript learning/Day 12 Regular Expression/Javascript/mainday12.js
+++ 30 Days JavaScript learning/Day 12 Regular Expression/Javascript/mainday12.js
@@ -8,3 +8,30 @@ let sumFirstSentence=extractFirstSentence.reduce((a,b)=>parseInt(a)+parseInt(b),
 // bisa di liat disini masi string
 console.log(extractFirstSentence);
 console.log(sumFirstSentence);
+// No 2
+let points = ['-1', '2', '-4', '-3', '-1', '0', '4', '8'];
+let numPoint=points.join(',').match(/-?\d+/g).map(Number)
+let sortedPoint=numPoint.sort((a,b)=>a-b)
+let distancePoint=sortedPoint[sortedPoint.length-1]-sortedPoint[0]
+console.log(sortedPoint);
+console.log(`distancePoint=${distancePoint}`);
+
+// No 3
+function validVariable(sentence) {
+    let pattern=/^[A-Za-z]*[A-Za-z_]*$/;
+    let result=pattern.test(sentence)
+    return result
+}
+console.log(validVariable('firstas_name'));
+console.log(validVariable('first-tname'));
+console.log(validVariable('1firstname'));
+console.log(validVariable('firstname'));
+
+// No 3 In Web
+function textCheckerWeb() {
+    let Text=document.getElementById('textChecker').value;
+    let Result=document.getElementById('resultCheck');
+    let pattern=/^[A-Za-z]*[a-zA-Z_]$/
+    let resultText=pattern.test(Text)
+    Result.innerHTML=`Hasil=${resultText}`
+}
