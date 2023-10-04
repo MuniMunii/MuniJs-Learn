b0f3a2ac5ce4a0d6a52ec8d37b37121ad18117e3
Ramzi_Akbar <110384679+MuniMunii@users.noreply.github.com>
2023-10-04 21:31:09 +0700

    Add files via upload

--- 30 Days JavaScript learning/Day 12 Regular Expression/Javascript/mainday12.js
+++ 30 Days JavaScript learning/Day 12 Regular Expression/Javascript/mainday12.js
@@ -35,3 +35,37 @@ function textCheckerWeb() {
     let resultText=pattern.test(Text)
     Result.innerHTML=`Hasil=${resultText}`
 }
+// No 4 masalah dan solusi di nomor ini
+let  paragraph = `I love teaching. If you do not love teaching what else can you love. I love Python if you do not love something which can give you all the capabilities to develop an application what else can you love.`;
+function mostFrequentWords(text,numSlice){
+    // pattern disini adalah \b untuk ngecek karakter apakah itu nomor,huruf,_ jika selain itu maka tidak akan dibaca
+    // dan [a-zA-Z]+ jika huruf yang di dalam [] lebih dari sekali kemunculan nya dan di tutup dengan \b lagi, gi itu artinya global dan case insensitive
+    let pattern=/\b[a-zA-Z]+\b/gi
+    // kita buat variable nya menjadi array agar bisa di baca kalimat nya pakai Array.from dan text.match(pattern)apakah sama dengan pattern nya
+    let extractArray=Array.from(text.match(pattern))
+    // kita hitung array nya makai reduce
+    let countArray=extractArray.reduce((count,text)=>{
+        count[text]=(count[text]||0)+1;
+        return count;
+    },{})
+    // kita ambil key nya dari count array dan kita jadikan object menggunakan map
+    let arrayKeys=Object.keys(countArray).map(Text=>({
+        Text,
+        count:countArray[Text]
+    })).slice(0,numSlice).sort((a,b)=>b.count-a.count)
+    return arrayKeys
+}
+console.log(mostFrequentWords(paragraph));
+
+// No 5 masalah disini 
+let sentence = `%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?`;
+function clearText(sentence) {
+    // kita cari pattern nya dulu [@#$%&;] kita masukan simbol unik nya
+    let pattern=/[@#$%&;]/gi
+    // abis itu kita replace pattern tadi dengan string kosong
+    let clearTexts=sentence.replace(pattern,'')
+    return clearTexts
+}
+console.log(clearText(sentence));
+// ini kita pakai double function dari no 4 tadi
+console.log(mostFrequentWords(clearText(sentence),3));
