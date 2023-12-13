2cb40604ea51bf79217b73e89f704cc2fbf36dd4
Ramzi_Akbar <110384679+MuniMunii@users.noreply.github.com>
2023-12-13 21:12:30 +0700

    Update Main.js

--- 30 Days JavaScript learning/Day 24 SolarSystem/Javascript/Main.js
+++ 30 Days JavaScript learning/Day 24 SolarSystem/Javascript/Main.js
@@ -1,5 +1,6 @@
-
-
+//if(getGravity==='merkurius'){
+img. style ganti
+}//
 let getResult=document.querySelector('.result')
 function calculateObject(){
     let getContent=document.querySelector('.content')
