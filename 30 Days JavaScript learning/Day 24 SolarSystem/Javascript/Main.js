72c3faa642134b9f4c25ba4c6490589fe4bfb39e
Ramzi_Akbar <110384679+MuniMunii@users.noreply.github.com>
2023-12-15 17:51:08 +0700

    Add files via upload

--- 30 Days JavaScript learning/Day 24 SolarSystem/Javascript/Main.js
+++ 30 Days JavaScript learning/Day 24 SolarSystem/Javascript/Main.js
@@ -1,22 +1,68 @@
 
-let getResult=document.querySelector('.result')
+// masalah dan solusi di nomor ini adalah
 function calculateObject(){
-    let getContent=document.querySelector('.content')
-    getContent.sty
+    // let getResult=document.querySelector('#result')
+    let getKotak=document.getElementById('kotak')
+    let getResultNotif=document.getElementById('result-notif')
+    let getResult=document.getElementById('result')
+    let getContent=document.getElementById('content')
     let getMass=parseFloat(document.getElementById('mass').value);
     let getPlanets=document.getElementById('planets')
+    // kita akan ambil value dari getPlanets
     let getPlanetsValue=getPlanets.value;
     if (getPlanetsValue) {
-        let getGravity = parseFloat(getPlanets.options[getPlanets.selectedIndex].getAttribute('data-gravity'));
-        if (!isNaN(getMass) && getMass !== '') {
-            let calculateObject = getMass * getGravity;
-            getResult.innerHTML = `Weight on ${getPlanetsValue}: ${calculateObject.toFixed(2)} N`;
-        } else if (isNaN(getMass)) {
-            getContent.innerHTML = 'Input the correct number for mass';
-        } else if (getMass === '') {
-            getContent.innerHTML = 'Input a valid mass';
+        // kita buat variable dengan isi optionsGetplanet[index yang dipilih dari get planet]dan mengambil dari atribut data-gravity
+        let getGravity=parseFloat(getPlanets.options[getPlanets.selectedIndex].getAttribute('data-gravity'));
+        // abis itu kita kalikan dengan mass yang ada di input htmlnya
+        let imgPath=document.querySelector('img')
+        let calculateObject=getMass*getGravity
+        getKotak.style.width='300px'
+        getKotak.style.height='250px'
+        getKotak.style.backgroundColor='#fff'
+        getContent.style.justifyContent='space-evenly'
+        getContent.style.alignItems='center'
+        getResultNotif.innerHTML=`The Weight Object On ${getPlanetsValue}`
+        getResult.innerHTML=`${calculateObject.toFixed(2)} N`
+        getResult.style.width='150px'
+        getResult.style.height='150px'
+        getResult.style.borderRadius='50%'
+        getResult.style.backgroundColor='pink'
+        if(getPlanetsValue==='merkurius'){
+            imgPath.src='../image/mercury.png'
         }
-    } else {
-        getContent.innerHTML = 'Select a planet';
+        else if(getPlanetsValue==='venus'){
+            imgPath.src='../image/venus.png'
+        }
+        else if(getPlanetsValue==='earth'){
+            imgPath.src='../image/earth.png'
+        }
+        else if(getPlanetsValue==='mars'){
+            imgPath.src='../image/mars.png'
+        }
+        else if(getPlanetsValue==='saturnus'){
+            imgPath.src='../image/saturn.png'
+        }
+        else if(getPlanetsValue==='jupiter'){
+            imgPath.src='../image/jupiter.png'
+        }
+        else if(getPlanetsValue==='uranus'){
+            imgPath.src='../image/uranus.png'
+        }
+        else if(getPlanetsValue==='neptunus'){
+            imgPath.src='../image/neptune.png'
+        }
+        else if(getPlanetsValue==='pluto'){
+            imgPath.src='../image/pluto.png'
+        }
+        else{
+            getResult.innerHTML='Select the Planets'
+        }
     }
+        if (isNaN(getMass)) {
+            getResult.innerHTML = 'Input the correct number for mass';
+            return;
+        } else if(getMass==='') {
+            getContent.innerHTML = 'input The valid mass';
+        }
+    
 }
