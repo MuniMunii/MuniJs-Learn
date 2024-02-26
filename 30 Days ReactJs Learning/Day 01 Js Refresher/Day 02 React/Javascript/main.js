5253c0221c08287980470e2fc8de8dbe4962df73
Ramzi_Akbar <110384679+MuniMunii@users.noreply.github.com>
2024-02-26 21:11:00 +0700

    Delete 30 Days ReactJs Learning/Day 01 Js Refresher directory

--- 30 Days ReactJs Learning/Day 01 Js Refresher/Day 02 React/Javascript/main.js
+++
@@ -1,9 +0,0 @@
-let Dummy='https://www.30daysofreact.com/dummy-data'
-fetch(Dummy)
-.then(Response=>Response.json())
-.then(Data=>{
-    console.log(Data);
-})
-.catch(error=>{
-    console.log(error);
-})
