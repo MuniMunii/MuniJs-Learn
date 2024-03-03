5fe5c5db0a87ef45df855affa16db1868e2969bd
Ramzi_Akbar <110384679+MuniMunii@users.noreply.github.com>
2024-03-03 21:12:48 +0700

    Add files via upload

---
+++ 30 Days ReactJs Learning/Day 06 Map Key List/src/index.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import  ReactDOM  from 'react-dom';
+import {Link} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ExerciseComponent from './exercise2';
+import ExerciseDay6 from './ExerciseDay6';
+const jsxElement=<h1>This is JSX Element</h1>
+const rootElement=document.getElementById('root')
+
+function Index(){
+  const HeaderStyle={
+    backgroundColor:'red',
+    fontFamily:'sans-serif',
+    padding:'25px',
+  }
+  const Wrapper={
+    margin:'0 auto',
+    width:'90%'
+  }
+  const elementBody=document.querySelector('body')
+  elementBody.style.margin='0'
+  const Header=(
+    <header style={{...HeaderStyle}}>
+      <div style={Wrapper}>
+      <h2>This is react</h2>
+      <h3>Muni Muni</h3>
+      <h4>Hello</h4>
+      <h4>Javascript Library</h4>
+      </div>
+    </header>
+  )
+  const Main=(
+    <div style={Wrapper}>
+      <p>Prerequisite Before react Learning</p>
+      <ul>
+        <li>Css</li>
+        <li>Html</li>
+        <li>Javascript</li>
+      </ul>
+    </div>
+  )
+  const Footer=(
+    <footer>
+      <h2>Copyright Muni 2024</h2>
+    </footer>
+  )
+  const App=(
+    <div>
+    {Header},
+    {Main},
+    {Footer}
+    </div>
+  )
+  return App
+}
+
+export default Index;
+
+ReactDOM.render(
+<React.StrictMode>
+<ExerciseDay6 />
+</React.StrictMode>,rootElement)
