682d566af26a52f962752196ff267a36ad8f3dd2
Ramzi_Akbar <110384679+MuniMunii@users.noreply.github.com>
2024-03-10 21:52:17 +0700

    Add files via upload

--- 30 Days ReactJs Learning/Day 09 Rendering/src/singlepageday.js
+++ 30 Days ReactJs Learning/Day 09 Rendering/src/singlepageday.js
@@ -11,17 +11,27 @@ class TimeApp extends React.Component{
         this.changeTime()
         this.changeTime=this.changeTime.bind(this)
     }
+    componentDidMount(){
+        const ONE_MINUTE=6000
+        this.interval=setInterval(this.changeTime,ONE_MINUTE)
+    }
+    componentWillUnmount(){
+        clearInterval(this.interval)
+    }
     changeTime(){
         const Dates=new Date()
-        this.state.time=Dates.getHours() < 12 ? 'AM':'PM'
+        this.setState({
+            time:Dates.getHours() < 12 ? 'AM':'PM',
+            emote:Dates.getHours() < 12?"🌅":'🌃'
+        })
     }
     render(){
         const Dates=new Date()
         const BodyElement=document.querySelector('body')
         BodyElement.setAttribute('class',this.state.time)
         return(
-            <div className={this.state.time}>
-                <p>Right now is {Dates.getHours()}:{Dates.getMinutes()} {this.state.time} {this.state.emote}</p>
+            <div className={this.state.time< 12 ? 'AM':'PM'}>
+                <p>Right now is {Dates.getHours()}:{Dates.getMinutes()} {this.state.time<12?'AM':'PM'} {this.state.emote}</p>
             </div>
         )
     }
