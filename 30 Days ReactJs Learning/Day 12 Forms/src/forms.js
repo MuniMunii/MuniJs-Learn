82ed0e6887ba40610f50409ff6949557637be5d9
Ramzi_Akbar <110384679+MuniMunii@users.noreply.github.com>
2024-04-15 21:58:45 +0700

    Add files via upload

--- 30 Days ReactJs Learning/Day 12 Forms/src/forms.js
+++ 30 Days ReactJs Learning/Day 12 Forms/src/forms.js
@@ -26,169 +26,38 @@ const DataCountry = [
 ];
 class FormsExample extends React.Component {
   state = {
-    firstName: {
-      firstNameValue: "",
-      isValid: false,
-    },
-    lastName: {
-      lastNameValue: "",
-      isValid: false,
-    },
-    email: {
-      emailValue: "",
-      isValid: false,
-    },
-    country: {
-      countryValue: "",
-      isValid: false,
-    },
-    tel: {
-      telValue: "",
-      isValid: false,
-    },
-    dateOfBirth: {
-      dateOfBirthValue: "",
-      isValid: false,
-    },
-    favoriteColor: {
-      colorValue: "",
-      isValid: false,
-    },
-    weight: {
-      weightValue: "",
-      isValid: false,
-    },
-    gender: {
-      genderValue: "",
-      isValid: false,
-    },
-    file: {
-      fileValue: "",
-      isValid: false,
-    },
-    bio: {
-      bioValue: "",
-      isValid: false,
-    },
+    firstName: "",
+    lastName: "",
+    email: "",
+    country: "",
+    tel: "",
+    dateOfBirth: "",
+    favoriteColor: "",
+    weight: "",
+    gender: "",
+    file: "",
+    bio: "",
     skills: {
       html: false,
       css: false,
       javascript: false,
     },
-    error: "",
   };
   handleChange = (e) => {
     const { name, value, type, checked } = e.target;
-    if (
-      e.target !== null ||
-      e.target !== "" ||
-      isNaN(e.target) ||
-      e.target !== undefined
-    ) {
-      if (type === "text") {
-        if (name === "firstName" || name === "lastName") {
-          if (value.length < 3 || value.length > 12) {
-            console.log("length Must be longer than 2 and shorter than 13");
-            this.setState((prevState) => ({
-              [name]: {
-                [name + "Value"]: value,
-                isValid: false,
-              },
-            }));
-          } else {
-            this.setState((prevState) => ({
-              [name]: {
-                [name + "Value"]: value,
-                isValid: true,
-              },
-            }));
-          }
-        }
-      } 
-      else if (type === "email") {
-        const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-        if (value.match(emailRegex)) {
-          this.setState((prevState) => ({
-            [name]: {
-              [name + "Value"]: value,
-              isValid: true,
-            },
-          }));
-        } else {
-          console.error("email is invalid");
-          this.setState((prevState) => ({
-            [name]: {
-              [name + "Value"]: value,
-              isValid: false,
-            },
-          }));
-        }
-      } 
-      else if (type === "checkbox") {
-        this.setState({
-          skills: { ...this.state.skills, [name]: checked },
-        });
-      }
-      else if(type==="number"){
-        if (name==='tel') {
-          const TelephonePattern=/^[0-9]{11,11}$/
-          if (value.match(TelephonePattern)) {
-            this.setState((prevState) => ({
-              [name]: {
-                [name + "Value"]: value,
-                isValid: true,
-              },
-            }));
-          }
-          else{ 
-            this.setState((prevState) => ({
-              [name]: {
-                [name + "Value"]: value,
-                isValid: false,
-              },
-            }))
-        }
-        }
-        else if(name==='weight'){
-          const WeightPattern=/^[0-9]$/
-          const NumericValue=value.match(WeightPattern)
-          if (value.match(WeightPattern)) {
-            this.setState((prevState) => ({
-              [name]: {
-                [name + "Value"]:value,
-                isValid: true,
-              },
-            }))
-          }
-          else{
-            this.setState((prevState) => ({
-              [name]: {
-                [name + "Value"]:value,
-                isValid: false,
-              },
-            }))
-          }
-        }
-      }
-      else if (type === "file") {
-        console.log("File type", e.target.files[0].name);
-        this.setState({
-          [name]: e.target.files[0],
-        });
-      } else {
-        this.setState(
-          (prevState) => (
-            console.log(prevState),
-            {
-              [name]: {
-                [name + "Value"]: value,
-                isValid: true,
-              },
-            }
-          )
-        );
-      }
+    if (type === "checkbox") {
+      this.setState({
+        skills: { ...this.state.skills, [name]: checked },
+      });
+    } else if (type === "file") {
+      console.log("File type", e.target.files[0].name); // Add this line
+      this.setState({
+        [name]: e.target.files[0],
+      });
     } else {
+      this.setState({
+        [name]: value,
+      });
     }
   };
   handleSubmit = (e) => {
@@ -213,7 +82,6 @@ class FormsExample extends React.Component {
         FormatedSkills.push(value.toUpperCase());
       }
     }
-    const FormattedWeight='Kg '+weight
     const Data = {
       firstName,
       lastName,
@@ -221,7 +89,7 @@ class FormsExample extends React.Component {
       tel,
       dateOfBirth,
       favoriteColor,
-      weight:FormattedWeight,
+      weight,
       country,
       gender,
       bio,
@@ -242,7 +110,6 @@ class FormsExample extends React.Component {
       country,
       gender,
       bio,
-      error,
     } = this.state;
     const SelectedCountry = DataCountry.map(({ value, label }) => (
       <option key={value} value={value}>
@@ -254,112 +121,95 @@ class FormsExample extends React.Component {
         <h1>test</h1>
         <form onSubmit={this.handleSubmit}>
           <div className="row">
-            <label htmlFor="firstName">First Name: </label>
-            <input
-              type="text"
-              name="firstName"
-              value={firstName.firstNameValue}
-              onChange={this.handleChange}
-              placeholder="First Name"
-              className="input-box"
-            />
-            <label htmlFor="firstName">{error}</label>
-          </div>
-          <div className="row">
-            <label htmlFor="lastName">Last Name:</label>
-            <input
-              type="text"
-              name="lastName"
-              value={lastName.lastNameValue}
-              placeholder="First Name"
-              onChange={this.handleChange}
-              className="input-box"
-            ></input>
-          </div>
-          <div className="row">
-            <label htmlFor="lastName">Email:</label>
-            <input
-              type="email"
-              name="email"
-              value={email.emailValue}
-              placeholder="Email"
-              onChange={this.handleChange}
-              className="input-box"
-            ></input>
-          </div>
-          <div className="row">
-            <label htmlFor="email">Telephone:</label>
-            <input
-              type="number"
-              name="tel"
-              value={tel.telValue}
-              placeholder="Telephone Number"
-              onChange={this.handleChange}
-              className="input-box"
-            ></input>
-          </div>
-          <div className="row">
-            <label htmlFor="dateOfBirth">Date Of Birth:</label>
-            <input
-              type="date"
-              name="dateOfBirth"
-              value={dateOfBirth.dateOfBirthValue}
-              placeholder="Date Of Birth"
-              onChange={this.handleChange}
-              className="input-box"
-            ></input>
-          </div>
-          <div className="row">
-            <label htmlFor="favoriteColor">favoriteColor:</label>
-            <input
-              type="text"
-              name="favoriteColor"
-              value={favoriteColor.colorValue}
-              placeholder="favoritecolor"
-              onChange={this.handleChange}
-              className="input-box"
-            ></input>
-          </div>
-          <div className="row">
-            <label htmlFor="weight">weight:</label>
-            <input
-              type="number"
-              name="weight"
-              value={weight.weightValue}
-              placeholder="weight"
-              onChange={this.handleChange}
-              className="input-box"
-            ></input>
-          </div>
-          <div className="row">
-            <label htmlFor="gender">Gender:</label>
-            <div>
-            <label htmlFor="gender">Male</label>
+            <div className="form-group">
+              <label htmlFor="firstName">First Name </label>
               <input
-              type="radio"
-              name="gender"
-              value="Male"
-              onChange={this.handleChange}
-              checked={gender.genderValue === "Male"}
-            ></input></div>
-            <div><label htmlFor="gender">Female</label>
-            <input
-              type="radio"
-              name="gender"
-              value="Female"
-              onChange={this.handleChange}
-              checked={gender.genderValue === "Female"}
-            ></input></div>
-            
-          </div>
-
+                type="text"
+                name="firstName"
+                value={firstName}
+                onChange={this.handleChange}
+                placeholder="First Name"
+                className="input-box"
+              />
+              </div>
+            </div>
+            <div className="row">
+              <label htmlFor="lastName">Last Name:</label>
+              <input
+                type="text"
+                name="lastName"
+                value={lastName}
+                placeholder="First Name"
+                onChange={this.handleChange}
+                className="input-box"
+              ></input>
+            </div>
+            <div className="row">
+              <label htmlFor="lastName">Email:</label>
+              <input
+                type="email"
+                name="email"
+                value={email}
+                placeholder="Email"
+                onChange={this.handleChange}
+                className="input-box"
+              ></input>
+            </div>
+            <div className="row">
+              <label htmlFor="email">Telephone:</label>
+              <input
+                type="number"
+                name="tel"
+                value={tel}
+                placeholder="Telephone Number"
+                onChange={this.handleChange}
+                className="input-box"
+              ></input>
+            </div>
+            <div className="row">
+              <label htmlFor="dateOfBirth">Date Of Birth:</label>
+              <input
+                type="date"
+                name="dateOfBirth"
+                value={dateOfBirth}
+                placeholder="Date Of Birth"
+                onChange={this.handleChange}
+                className="input-box"
+              ></input>
+            </div>
+            <div className="row">
+              <label htmlFor="favoriteColor">favoriteColor:</label>
+              <input
+                type="text"
+                name="favoriteColor"
+                value={favoriteColor}
+                placeholder="favoritecolor"
+                onChange={this.handleChange}
+                className="input-box"
+              ></input>
+            </div>
+            <div className="row">
+              <label htmlFor="weight">weight:</label>
+              <input
+                type="number"
+                name="weight"
+                value={weight}
+                placeholder="weight"
+                onChange={this.handleChange}
+                className="input-box"
+              ></input>
+            </div>
+            <div className="row">
+              <label htmlFor="gender">Gender:</label>
+              <label htmlFor="gender">Male</label>
+              <input type="radio" name="gender" value="Male" onChange={this.handleChange} checked={gender==='Male'}></input>
+              <label htmlFor="gender">Female</label>
+              <input type="radio" name="gender" value="Female" onChange={this.handleChange} checked={gender==='Female'}></input>
+            </div>
+          
           <div className="row">
             <label htmlFor="country">Country:</label>
-            <select
-              name="country"
-              value={country.countryValue}
-              onChange={this.handleChange}
-            >
+            <select name="country" value={country} onChange={this.handleChange}>
               {SelectedCountry}
             </select>
           </div>
